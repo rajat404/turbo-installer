527dbc4e2fc619a8865828526399c573329a69d0
rajat404 <404rajat@gmail.com>
2015-07-01 16:22:27 +0530

    bugs fixed & separate script for ZSH

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 # http://itsfoss.com/things-to-do-after-installing-ubuntu-14-04/
 gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 
-# xargs sudo add-apt-repository -y < ppa-requirements.txt
 sudo add-apt-repository -y ppa:webupd8team/nemo
 sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
 sudo add-apt-repository -y ppa:keithw/mosh
@@ -17,21 +16,9 @@ xargs sudo apt-get install -y < apt-requirements.txt
 
 sudo pip install -r pip-requirements.txt --upgrade
 
-#echo "Downloading sublime-text3"
-#FILE=`mktemp`
-#URL=`python latest_sublime_dl.py`
-#wget $URL -O $FILE
-#echo "Installing sublime text"
-#sudo dpkg -i $FILE
-
-# To start ZSH shell
-zsh
-echo "Setting up Prezto"
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-
-chsh -s /bin/zsh
+echo "Downloading sublime-text3"
+FILE=`mktemp`
+URL=`python latest_sublime_dl.py`
+wget $URL -O $FILE
+echo "Installing sublime text"
+sudo dpkg -i $FILE
---
+++ install_zsh.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "Setting up Prezto"
+# Visit http://jr0cket.co.uk/2013/09/hey-prezto-zsh-for-command-line-heaven.html for any issues
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin
+ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout
+ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
+ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
+ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
+ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
+
+zsh
+chsh -s /usr/bin/zsh
