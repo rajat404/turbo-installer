e3da935161b4f9ef4c55b19250edb236bcaea944
rajat404 <404rajat@gmail.com>
2015-07-01 13:45:48 +0530

    bugs fixed

--- install.sh
+++ install.sh
@@ -1,26 +1,28 @@
 #! /bin/bash
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-sudo sed -i 's/in.archive.ubuntu.com/mirror.dhakacom.com/' /etc/apt/sources.list
 
 # Disable shopping suggestions
 # http://itsfoss.com/things-to-do-after-installing-ubuntu-14-04/
 gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 
-xargs sudo add-apt-repository -y < ppa-requirements.txt
-sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get install -y boot-repair
+# xargs sudo add-apt-repository -y < ppa-requirements.txt
+sudo add-apt-repository -y ppa:webupd8team/nemo
+sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
+sudo add-apt-repository -y ppa:keithw/mosh
+sudo add-apt-repository -y ppa:videolan/stable-daily
+sudo add-apt-repository -y ppa:yannubuntu/boot-repair
+
+sudo apt-get -y update && sudo apt-get -y dist-upgrade
 
 xargs sudo apt-get install -y < apt-requirements.txt
 
 sudo pip install -r pip-requirements.txt --upgrade
 
-echo "Downloading sublime-text3"
-FILE=`mktemp`
-URL=`python latest_sublime_dl.py`
-wget $URL -O $FILE
-echo "Installing sublime text"
-sudo dpkg -i $FILE
+#echo "Downloading sublime-text3"
+#FILE=`mktemp`
+#URL=`python latest_sublime_dl.py`
+#wget $URL -O $FILE
+#echo "Installing sublime text"
+#sudo dpkg -i $FILE
 
 # To start ZSH shell
 zsh
